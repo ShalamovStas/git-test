3b0b2f2cbd0d723269725e3dbc6e175be6da4e25
stanislav.shalamov@ssa.group <44943979+ShalamovStas@users.noreply.github.com>
2022-05-31 21:34:28 +0300

    [stash-test]: Merge applied

--- how-to-run-ts/stash-test/script.js
+++ how-to-run-ts/stash-test/script.js
@@ -2,4 +2,10 @@ console.log('"object" :>> ', "object");
 
 let title = "Init Feature";
 
+<<<<<<< Updated upstream
 console.log(title);
+=======
+console.log(title);
+console.log("You can edit profile");
+//my test line
+>>>>>>> Stashed changes
