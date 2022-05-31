eef2c70624d573a60c6d9937da1f707abc86b97f
stanislav.shalamov@ssa.group <44943979+ShalamovStas@users.noreply.github.com>
2022-05-31 21:35:18 +0300

    [stash-test] Remove merge lines

--- how-to-run-ts/stash-test/script.js
+++ how-to-run-ts/stash-test/script.js
@@ -2,10 +2,7 @@ console.log('"object" :>> ', "object");
 
 let title = "Init Feature";
 
-<<<<<<< Updated upstream
 console.log(title);
-=======
 console.log(title);
 console.log("You can edit profile");
 //my test line
->>>>>>> Stashed changes
